c3baaf2077d17d39316aa58b7d6f457529ceb463
htiwary2 <htiwary2@illinois.edu>
2022-12-18 18:14:26 -0500

    PKCE OAuth attempt

---
+++ app.js
@@ -0,0 +1,118 @@
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const https = require('https');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+
+const fs = require('fs');
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+
+let rawdata = fs.readFileSync('keys.json');
+let keys = JSON.parse(rawdata)
+
+var client_id = keys['CLIENT_ID'];
+var client_secret = keys['CLIENT_SECRET'];
+var redirect_uri = keys['REDIRECT_URI'];
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+async function generateCodeChallenge(code_verifier) {
+  const digest = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(code_verifier));
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+var stateKey = 'spotify_auth_state';
+const code_verifier = generateRandomString(64);
+
+var app = express();
+server = https.createServer({key: key, cert: cert }, app);
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
+
+app.get('/', (req, res) => { res.send('this is a secure server') });
+
+app.get('/login', function(req, res) {
+  generateCodeChallenge(code_verifier).then((code_challenge) => {
+    window.localStorage.setItem('code_verifier', code_verifier);
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: 'user-read-private user-read-email',
+        redirect_uri: redirect_uri,
+        state: generateRandomString(16),
+        code_challenge_method: "S256",
+        code_challenge,
+    }));
+  }).catch((err) => {console.log(err)});
+});
+
+app.get('/callback', function(req, res) {
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  if (state === null) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code',
+        client_id: client_id,
+        code_verifier: code_verifier
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token,
+      client_id: client_id
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+server.listen(8888, () => { console.log('listening on 8888') });
+console.log(crypto.subtle)
