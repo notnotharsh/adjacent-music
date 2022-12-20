fa3f4e0221f3c97471a25200853a5e89f213e56c
notnotharsh <unagctystudent2@gmail.com>
2022-12-20 10:19:53 -0500

    PKCE fixes

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 
 const fs = require('fs');
-const key = fs.readFileSync('./key.pem');
-const cert = fs.readFileSync('./cert.pem');
 
 let rawdata = fs.readFileSync('keys.json');
 let keys = JSON.parse(rawdata)
 
 var client_id = keys['CLIENT_ID'];
-var client_secret = keys['CLIENT_SECRET'];
 var redirect_uri = keys['REDIRECT_URI'];
 
 var generateRandomString = function(length) {
@@ -44,32 +41,35 @@ app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
 app.get('/login', function(req, res) {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
   generateCodeChallenge(code_verifier).then((code_challenge) => {
-    window.localStorage.setItem('code_verifier', code_verifier);
     res.redirect('https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: client_id,
-        scope: 'user-read-private user-read-email',
+        scope: 'user-read-private user-top-read',
         redirect_uri: redirect_uri,
-        state: generateRandomString(16),
+        state: state,
         code_challenge_method: "S256",
         code_challenge,
     }));
-  }).catch((err) => {console.log(err)});
+  }).catch((err) => {res.redirect("/login")});
 });
 
 app.get('/callback', function(req, res) {
 
   var code = req.query.code || null;
   var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  if (state === null) {
+  if (state === null || state !== storedState) {
     res.redirect('/#' +
       querystring.stringify({
         error: 'state_mismatch'
       }));
   } else {
+    res.clearCookie(stateKey);
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
@@ -79,11 +79,37 @@ app.get('/callback', function(req, res) {
         client_id: client_id,
         code_verifier: code_verifier
       },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
       json: true
     };
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
   }
 });
 
@@ -92,24 +118,23 @@ app.get('/refresh_token', function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token,
-      client_id: client_id
-    },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    form: {grant_type: 'refresh_token', refresh_token: refresh_token, client_id: client_id},
     json: true
   };
-
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
+      var refresh_token = body.refresh_token;
+      res.send({'access_token': access_token, 'refresh_token': refresh_token});
+    } else {
+      res.redirect("/login");
     }
   });
 });
 
-console.log('listening on 8888');
-app.listen(8888);
+app.listen(8888, () => {
+  console.log("listening on port 8888...");
+});
+
+module.exports = app;
